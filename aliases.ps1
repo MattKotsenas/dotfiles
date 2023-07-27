5f42d85ebd9284c23e50a1f980eccaf4dcac9706
Matt Kotsenas <matt.kotsenas@gmail.com>
2023-07-27 09:06:12 -0700

    Fix aliases so they are idempotent when refreshing env

--- aliases.ps1
+++ aliases.ps1
@@ -1,6 +1,6 @@
-New-Alias -Name say -Value Out-Speech
-New-Alias -Name spell -Value Get-SpellingSuggestions
-New-Alias -Name open -Value Invoke-Item
-New-Alias -Name dict -Value Get-Definition
-New-Alias -Name n -Value notepad
-New-Alias -Name edit -Value code
+Set-Alias -Name say -Value Out-Speech
+Set-Alias -Name spell -Value Get-SpellingSuggestions
+Set-Alias -Name open -Value Invoke-Item
+Set-Alias -Name dict -Value Get-Definition
+Set-Alias -Name n -Value notepad
+Set-Alias -Name edit -Value code
