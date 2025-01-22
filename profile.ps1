c883db87d068052826193b71bdba20ca1cbe1fe3
Matt Kotsenas <matt.kotsenas@gmail.com>
2025-01-21 22:10:25 -0800

    Add posh-git and oh-my-posh to profile

--- profile.ps1
+++ profile.ps1
@@ -1,3 +1,6 @@
+Import-Module posh-git
+Import-Module oh-my-posh
+
 foreach ($scriptFile in (Get-ChildItem -Path $PSScriptRoot -Recurse -Include *.ps1 -Exclude profile.ps1))
 {
   . $scriptFile.FullName
