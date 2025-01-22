490a9f775ae3c52e442eb3fafd163d146a3bc0de
Matt Kotsenas <matt.kotsenas@gmail.com>
2025-01-21 22:10:25 -0800

    Replace thefuck's function with built-in alias
    - Delete the function for thefuck + the alias
    - Invoke thefuck's built-in alias

--- scripts/Get-TheFuckSuggestions.ps1
+++
@@ -1,8 +0,0 @@
-function Get-TheFuckSuggestions {
-    $fuck = $(thefuck (Get-History -Count 1).CommandLine)
-    if (-not [string]::IsNullOrWhiteSpace($fuck))
-    {
-        if ($fuck.StartsWith("echo")) { $fuck = $fuck.Substring(5) }
-        else { iex "$fuck" }
-    }
-}
