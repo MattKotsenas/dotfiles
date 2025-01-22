b7203607c1ebda7d2db4b0eb10188692c056008b
Matt Kotsenas <matt.kotsenas@gmail.com>
2025-01-21 22:10:23 -0800

    Add Update-EnvironmentVariables
    - Add function to refresh the environment variables in the current session
      (useful for chaining Chocolatey / Boxstarter calls together)

---
+++ scripts/Update-EnvironmentVariables.ps1
@@ -0,0 +1,14 @@
+function Update-EnvironmentVariables
+{
+    foreach($level in "Machine","User")
+    {
+        [Environment]::GetEnvironmentVariables($level).GetEnumerator() | % {
+            # For Path variables, append the new values, if they're not already in there
+            if($_.Name -match 'Path$')
+            {
+                $_.Value = ($((Get-Content "Env:$($_.Name)") + ";$($_.Value)") -split ';' | Select -unique) -join ';'
+            }
+            $_
+        } | Set-Content -Path { "Env:$($_.Name)" }
+    }
+}
