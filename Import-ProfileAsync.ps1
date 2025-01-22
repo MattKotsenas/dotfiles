883c098356304fb23b97aaea91e747aa0fdb9dff
Matt Kotsenas <matt.kotsenas@gmail.com>
2025-01-21 22:10:33 -0800

    Make module import async

---
+++ Import-ProfileAsync.ps1
@@ -0,0 +1,64 @@
+# Adapted from https://fsackur.github.io/2023/11/20/Deferred-profile-loading-for-better-performance/
+
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [ScriptBlock]
+    $Deferred
+)
+
+# https://seeminglyscience.github.io/powershell/2017/09/30/invocation-operators-states-and-scopes
+$GlobalState = [psmoduleinfo]::new($false)
+$GlobalState.SessionState = $ExecutionContext.SessionState
+
+# to run our code asynchronously
+$Runspace = [runspacefactory]::CreateRunspace($Host)
+$Powershell = [powershell]::Create($Runspace)
+$Runspace.Open()
+$Runspace.SessionStateProxy.PSVariable.Set('GlobalState', $GlobalState)
+
+# ArgumentCompleters are set on the ExecutionContext, not the SessionState
+# Note that $ExecutionContext is not an ExecutionContext, it's an EngineIntrinsics
+$Private = [Reflection.BindingFlags]'Instance, NonPublic'
+$ContextField = [Management.Automation.EngineIntrinsics].GetField('_context', $Private)
+$Context = $ContextField.GetValue($ExecutionContext)
+
+# Get the ArgumentCompleters. If null, initialise them.
+$ContextCACProperty = $Context.GetType().GetProperty('CustomArgumentCompleters', $Private)
+$ContextNACProperty = $Context.GetType().GetProperty('NativeArgumentCompleters', $Private)
+$CAC = $ContextCACProperty.GetValue($Context)
+$NAC = $ContextNACProperty.GetValue($Context)
+if ($null -eq $CAC)
+{
+    $CAC = [Collections.Generic.Dictionary[string, scriptblock]]::new()
+    $ContextCACProperty.SetValue($Context, $CAC)
+}
+if ($null -eq $NAC)
+{
+    $NAC = [Collections.Generic.Dictionary[string, scriptblock]]::new()
+    $ContextNACProperty.SetValue($Context, $NAC)
+}
+
+# Get the AutomationEngine and ExecutionContext of the runspace
+$RSEngineField = $Runspace.GetType().GetField('_engine', $Private)
+$RSEngine = $RSEngineField.GetValue($Runspace)
+$EngineContextField = $RSEngine.GetType().GetFields($Private) | Where-Object {$_.FieldType.Name -eq 'ExecutionContext'}
+$RSContext = $EngineContextField.GetValue($RSEngine)
+
+# Set the runspace to use the global ArgumentCompleters
+$ContextCACProperty.SetValue($RSContext, $CAC)
+$ContextNACProperty.SetValue($RSContext, $NAC)
+
+$Wrapper = {
+    # Without a sleep, you get issues:
+    #   - occasional crashes
+    #   - prompt not rendered
+    #   - no highlighting
+    # Assumption: this is related to PSReadLine.
+    # 20ms seems to be enough on my machine, but let's be generous - this is non-blocking
+    #Start-Sleep -Milliseconds 200
+
+    . $GlobalState {. $Deferred; Remove-Variable Deferred}
+}
+
+$null = $Powershell.AddScript($Wrapper.ToString()).BeginInvoke()
--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,24 +1,35 @@
-oh-my-posh init pwsh --config $env:POSH_THEMES_PATH/jandedobbeleer.omp.json | Invoke-Expression
-$env:POSH_GIT_ENABLED = $true
+# Ensure delay loaded modules are available
+# TODO: Remove this once errors are handled in async
+if (-not (Get-Module -Name z -ListAvailable)) { throw "z not installed" }
 
+# Import modules that don't work with async profile import
+# TODO: Get this to work with async profile
 Import-Module -Name Terminal-Icons
 
-Import-Module z
+# TODO: Fix caret when inside async
+oh-my-posh init pwsh --config $Env:POSH_THEMES_PATH/jandedobbeleer.omp.json | Invoke-Expression
+$Env:POSH_GIT_ENABLED = $true
+
+. $PSScriptRoot\Import-ProfileAsync.ps1 -Deferred {
+    # throw "foo"
+    $Env:PYTHONIOENCODING='utf-8'
+    iex "$(thefuck --alias)"
+
+    Import-Module z
+
+    # PowerShell parameter completion shim for the dotnet CLI
+    Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
+        param($wordToComplete, $commandAst, $cursorPosition)
+            dotnet complete --position $cursorPosition "$commandAst" | ForEach-Object {
+                [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+            }
+    }
+}
 
 # Load up everything in the scripts folder
+# TODO: Move into async
 foreach ($scriptFile in (Get-ChildItem -Path $PSScriptRoot\scripts -Recurse -Include *.ps1))
 {
   . $scriptFile.FullName
 }
 . $PSScriptRoot\aliases.ps1
-
-$Env:PYTHONIOENCODING='utf-8'
-iex "$(thefuck --alias)"
-
-# PowerShell parameter completion shim for the dotnet CLI
-Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
-    param($wordToComplete, $commandAst, $cursorPosition)
-        dotnet complete --position $cursorPosition "$commandAst" | ForEach-Object {
-            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
-        }
-}
