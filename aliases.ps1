120641a6a0fc14ca6b993e3c43158150ff98e98c
Matt Kotsenas <matt.kotsenas@gmail.com>
2025-01-21 22:10:26 -0800

    Updates
    Start to merge the different PowerShell profile repos together
    
    - Rename profile so it only works on PowerShell sessions
    - Limit script loading to scripts/ folder so modules aren't
    automatically processed
    - Add n -> notepad alias

--- aliases.ps1
+++ aliases.ps1
@@ -2,3 +2,4 @@ New-Alias -Name say -Value Out-Speech
 New-Alias -Name spell -Value Get-SpellingSuggestions
 New-Alias -Name open -Value Invoke-Item
 New-Alias -Name dict -Value Get-Definition
+New-Alias -Name n -Value notepad
