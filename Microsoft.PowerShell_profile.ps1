df364cb89440e76d0eabf9504c8d5279002b1cdb
Matt Kotsenas <matt.kotsenas@gmail.com>
2025-01-21 22:10:35 -0800

    Use idle event for async init

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -8,21 +8,27 @@
     }
 }
 
-# Import modules that don't work with async profile import
-# TODO: Get this to work with async profile
-Import-Module -Name Terminal-Icons
-
-function prompt {
-    # oh-my-posh will override this prompt, however we want to customize it to prevent
-    # PowerShell's own customization from being added.
-
-    "[Loading...]: PS $($executionContext.SessionState.Path.CurrentLocation)$('>' * ($nestedPromptLevel + 1)) ";
+# Load up everything in the scripts folder
+foreach ($script in Get-ChildItem (Join-Path $PSScriptRoot scripts *.ps1))
+{
+    . $script.FullName
 }
 
-. $PSScriptRoot\Import-ProfileAsync.ps1 -Deferred {
-    # Note: Load items in priority order
+# Disable the "make the prompt red during parse error" because it conflicts with oh-my-posh
+Set-PSReadLineOption -PromptText ''
+function prompt {
+    # oh-my-posh will override this prompt, however because we're loading it async we want communicate that the
+    # real prompt is still loading.
+    "[async init]: PS $($executionContext.SessionState.Path.CurrentLocation)$('>' * ($nestedPromptLevel + 1)) ";
+}
+
+# Load modules asynchronously to improve shell startup
+$null = Register-EngineEvent -SourceIdentifier PowerShell.OnIdle -MaxTriggerCount 1 -Action {
     oh-my-posh init pwsh --config (Join-Path (Split-Path $PROFILE) matt.omp.json) | Invoke-Expression
     $Env:POSH_GIT_ENABLED = $true
+
+    Import-Module -Name Terminal-Icons
+    Import-Module -Name z
 
     # PowerShell parameter completion shim for the dotnet CLI
     Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
@@ -31,22 +37,6 @@ function prompt {
                 [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
             }
     }
-
-    # Load up everything in the scripts folder
-    $root = (Split-Path $PROFILE)
-    . $root\scripts\beep.ps1
-    . $root\scripts\Get-Clipboard.ps1
-    . $root\scripts\Get-Definition.ps1
-    . $root\scripts\Get-SpellingSuggestions.ps1
-    . $root\scripts\Get-Synonym.ps1
-    . $root\scripts\hiyo.ps1
-    . $root\scripts\horns.aiff.ps1
-    . $root\scripts\New-Gitignore.ps1
-    . $root\scripts\Out-Speech.ps1
-    . $root\scripts\tail.ps1
-    . $root\scripts\Update-EnvironmentVariables.ps1
-    . $root\scripts\which.ps1
-    . $root\aliases.ps1
 
     $Env:PYTHONIOENCODING='utf-8'
     iex "$(thefuck --alias)"
