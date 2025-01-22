f9648be9b3fb59c20b928b3d9699751e9b7ef643
Matt Kotsenas <matt.kotsenas@gmail.com>
2025-01-21 22:10:26 -0800

    Add dit -> code alias

--- aliases.ps1
+++ aliases.ps1
@@ -3,3 +3,4 @@ New-Alias -Name spell -Value Get-SpellingSuggestions
 New-Alias -Name open -Value Invoke-Item
 New-Alias -Name dict -Value Get-Definition
 New-Alias -Name n -Value notepad
+New-Alias -Name edit -Value code
