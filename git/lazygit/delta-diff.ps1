1ae81388dcf333b5bd1b2ec3ba788f4672b15a14
Matt Kotsenas <matt.kotsenas@gmail.com>
2025-12-17 10:30:43 -0800

    Use renaming for lazygit delta diff

--- git/lazygit/delta-diff.ps1
+++ git/lazygit/delta-diff.ps1
@@ -1,2 +1,10 @@
 #!/usr/bin/env pwsh
-git diff --no-index --no-ext-diff $args[1] $args[4] | delta
+
+# From https://github.com/jesseduffield/lazygit/pull/4941/changes
+
+$old = $args[1].Replace('\', '/')
+$new = $args[4].Replace('\', '/')
+$path = $args[0]
+git diff --no-index --no-ext-diff $old $new
+  | %{ $_.Replace($old, $path).Replace($new, $path) }
+  | delta --hyperlinks
