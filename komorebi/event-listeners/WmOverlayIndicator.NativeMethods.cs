8d4740676beab4f1824f8346d2d4a49e4a2ffdbd
Matt Kotsenas <51421+MattKotsenas@users.noreply.github.com>
2026-05-21 17:16:31 -0700

    refactor: extract overlay P/Invoke to partial class, add SupportedOSPlatform

---
+++ komorebi/event-listeners/WmOverlayIndicator.NativeMethods.cs
@@ -0,0 +1,105 @@
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+namespace EventListeners;
+
+/// <summary>
+/// Win32 P/Invoke declarations for <see cref="WmOverlayIndicator"/>.
+/// </summary>
+[SupportedOSPlatform("windows")]
+public sealed partial class WmOverlayIndicator
+{
+    private const uint CS_HREDRAW = 0x0002;
+    private const uint CS_VREDRAW = 0x0001;
+    private const uint WS_POPUP = 0x80000000;
+    private const uint WS_EX_TOPMOST = 0x00000008;
+    private const uint WS_EX_TOOLWINDOW = 0x00000080;
+    private const uint WS_EX_LAYERED = 0x00080000;
+    private const uint WS_EX_TRANSPARENT = 0x00000020;
+    private const uint WS_EX_NOACTIVATE = 0x08000000;
+    private const uint WM_PAINT = 0x000F;
+    private const uint WM_CLOSE = 0x0010;
+    private const uint WM_DESTROY = 0x0002;
+    private const int SW_HIDE = 0;
+    private const int SW_SHOWNOACTIVATE = 4;
+    private const int SM_CXSCREEN = 0;
+    private const int TRANSPARENT = 1;
+    private const uint LWA_ALPHA = 0x02;
+    private const int FW_BOLD = 700;
+    private const uint DT_CENTER = 0x01;
+    private const uint DT_VCENTER = 0x04;
+    private const uint DT_SINGLELINE = 0x20;
+    private static readonly nint IDC_ARROW = 32512;
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+    private struct WNDCLASSEX
+    {
+        public uint cbSize;
+        public uint style;
+        [MarshalAs(UnmanagedType.FunctionPtr)] public WndProcDelegate lpfnWndProc;
+        public int cbClsExtra;
+        public int cbWndExtra;
+        public nint hInstance;
+        public nint hIcon;
+        public nint hCursor;
+        public nint hbrBackground;
+        public string? lpszMenuName;
+        public string lpszClassName;
+        public nint hIconSm;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    private struct PAINTSTRUCT
+    {
+        public nint hdc;
+        public bool fErase;
+        public RECT rcPaint;
+        public bool fRestore;
+        public bool fIncUpdate;
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 32)] public byte[] rgbReserved;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    private struct RECT { public int left, top, right, bottom; }
+
+    [StructLayout(LayoutKind.Sequential)]
+    private struct MSG
+    {
+        public nint hwnd;
+        public uint message;
+        public nint wParam;
+        public nint lParam;
+        public uint time;
+        public POINT pt;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    private struct POINT { public int x, y; }
+
+    private delegate nint WndProcDelegate(nint hwnd, uint msg, nint wParam, nint lParam);
+
+    [DllImport("user32.dll", CharSet = CharSet.Unicode)] private static extern ushort RegisterClassEx(ref WNDCLASSEX lpwcx);
+    [DllImport("user32.dll", CharSet = CharSet.Unicode)] private static extern nint CreateWindowEx(uint exStyle, string className, string windowName, uint style, int x, int y, int width, int height, nint parent, nint menu, nint instance, nint param);
+    [DllImport("user32.dll")] private static extern bool ShowWindow(nint hwnd, int cmdShow);
+    [DllImport("user32.dll")] private static extern bool SetLayeredWindowAttributes(nint hwnd, uint crKey, byte bAlpha, uint dwFlags);
+    [DllImport("user32.dll")] private static extern int GetSystemMetrics(int index);
+    [DllImport("user32.dll")] private static extern nint LoadCursor(nint hInstance, nint lpCursorName);
+    [DllImport("kernel32.dll", CharSet = CharSet.Unicode)] private static extern nint GetModuleHandle(string? lpModuleName);
+    [DllImport("user32.dll")] private static extern int GetMessage(out MSG msg, nint hwnd, uint filterMin, uint filterMax);
+    [DllImport("user32.dll")] private static extern bool TranslateMessage(ref MSG msg);
+    [DllImport("user32.dll")] private static extern nint DispatchMessage(ref MSG msg);
+    [DllImport("user32.dll")] private static extern bool PostMessage(nint hwnd, uint msg, nint wParam, nint lParam);
+    [DllImport("user32.dll")] private static extern void PostQuitMessage(int exitCode);
+    [DllImport("user32.dll")] private static extern nint DefWindowProc(nint hwnd, uint msg, nint wParam, nint lParam);
+    [DllImport("user32.dll")] private static extern nint BeginPaint(nint hwnd, ref PAINTSTRUCT ps);
+    [DllImport("user32.dll")] private static extern bool EndPaint(nint hwnd, ref PAINTSTRUCT ps);
+    [DllImport("user32.dll")] private static extern bool GetClientRect(nint hwnd, out RECT rect);
+    [DllImport("user32.dll")] private static extern int FillRect(nint hdc, ref RECT rect, nint hbr);
+    [DllImport("gdi32.dll")] private static extern nint CreateSolidBrush(uint color);
+    [DllImport("gdi32.dll")] private static extern bool DeleteObject(nint obj);
+    [DllImport("gdi32.dll")] private static extern int SetBkMode(nint hdc, int mode);
+    [DllImport("gdi32.dll")] private static extern uint SetTextColor(nint hdc, uint color);
+    [DllImport("gdi32.dll", CharSet = CharSet.Unicode)] private static extern nint CreateFont(int height, int width, int escapement, int orientation, int weight, uint italic, uint underline, uint strikeOut, uint charSet, uint outPrecision, uint clipPrecision, uint quality, uint pitchAndFamily, string face);
+    [DllImport("gdi32.dll")] private static extern nint SelectObject(nint hdc, nint obj);
+    [DllImport("user32.dll", CharSet = CharSet.Unicode)] private static extern int DrawText(nint hdc, string text, int count, ref RECT rect, uint format);
+}
